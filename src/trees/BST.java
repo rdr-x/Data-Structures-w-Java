1c1067a4affd7ea37d9e0a384253db7edade4bc7
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2023-03-25 14:12:57 -0600

    initialize BST and node files

---
+++ src/trees/BST.java
@@ -0,0 +1,15 @@
+package trees;
+
+public class BST {
+
+    public BST() {
+
+    }
+    public void addNode(Node _node){}
+    public void removeNode(){}
+    public void preorder(){}
+    public void inorder(){}
+    public void postorder(){}
+    public void getElements(){}
+
+}
