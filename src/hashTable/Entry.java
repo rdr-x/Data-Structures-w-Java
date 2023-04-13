8ee1ea9be854ba6bbe2d25afdca443aae2f43a05
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2023-04-13 00:20:14 -0600

    feat: adding first methods to hash table | simple

--- src/hashTable/Entry.java
+++ src/hashTable/Entry.java
@@ -1,5 +1,10 @@
 package hashTable;
 
 public class Entry {
-
+    public String key;
+    public String data;
+    public Entry(String _key, String _value) {
+        this.key = _key;
+        this.data = _value;
+    }
 }
--- src/hashTable/HashTable.java
+++ src/hashTable/HashTable.java
@@ -1,8 +1,47 @@
 package hashTable;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class HashTable {
 
-    public HashTable() {
+    int size, elements, FC;
+    private ArrayList<Entry> table;
 
+    public HashTable() {
+        this.table = new ArrayList<Entry>(randomPrimeNumber());
+        this.size = table.size();
+        this.elements = 0;
+        this.FC = 0;
+    }
+    private int hashing(String _key) { return 0; }
+    public Entry search(String _key) {
+        int index = hashing(_key);
+        return table.get(index);
+    }
+    public void addEntry(Entry _entry){
+        int index = hashing(_entry.key);
+        this.table.add(index,_entry);
+        System.out.println(_entry.key + "ADDED");
+    }
+    public Entry removeEntry(String _key){
+        int index = hashing(_key);
+        Entry element = this.table.get(index);
+        this.table.remove(index);
+        return element;
+    }
+    private void fragmentation() {}
+    private void inverseFragmentation(){}
+    private boolean isPrimeNumber(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+    private int randomPrimeNumber() {
+        Random rand = new Random();
+        while (true) {
+            int randomNumber = rand.nextInt(100);
+            if (isPrimeNumber(randomNumber)) return randomNumber;
+        }
     }
 }
